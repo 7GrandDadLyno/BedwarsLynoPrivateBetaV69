4a222447dc3da9831d7dc8ee319776fe2d38aa19
7GrandDadVape <127914282+7GrandDadLyno@users.noreply.github.com>
2023-03-24 15:21:27 +0800

    Create MainScript.lua

---
+++ ForRoblox/7GrandDadLynoOfficalFile/FileProtected/Private/Beta/MainScript.lua
@@ -0,0 +1,5 @@
+--File Protected By 7GrandDadLyno#0001--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadLyno/BedwarsLynoPrivateBetaV69/main/ForRoblox/7GrandDadLynoOfficalFile/FileProtected/Private/Beta/69.lua"))()
+kill = local.script(CloseUI)[True]
+if nil "--"+"File Protected By 7GrandDadLyno#0001"+"--":
+  kill[true]
